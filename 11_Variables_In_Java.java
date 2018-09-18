e669d4b84959f44158ab19db03226150314c06bd
Priyank Gada <kustom.hackers@gmail.com>
2018-09-18 16:40:12 +0530

    Create 11_Variables_In_Java.java

---
+++ 11_Variables_In_Java.java
@@ -0,0 +1,36 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+  
+  	int a = 20; //Only Whole Numbers or integers
+  
+  	
+  	double age;
+  	age = 20.1; //Double is whole or decimal number
+  	
+  	boolean Life; //Boolean is only true or false
+  	Life = true;
+  	
+  	String Name;
+  	Name = "Priyank";
+  	
+  	
+  	System.out.println(Name);
+  	System.out.println("My Name is " + Name + " and I am " + a + " Years old.");  
+  	
+  	System.out.println( a + age - 7.4);
+  	
+  	System.out.println(Name.length());
+  	
+  	String FirstName;
+  	FirstName = "Priyank";
+  	
+  	String SecondName;
+  	SecondName = "Gada";
+  	
+  	int length = FirstName.length() + SecondName.length();
+  	System.out.println(length);
+  
+  
+   
+  }
+}
